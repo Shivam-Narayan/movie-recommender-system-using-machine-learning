fb05365a00d9c801e8b14ca18ffd307a04766ed9
Shivam Narayan <sshivam6495@gmail.com>
2025-02-12 17:07:14 +0000

    streamlit app

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import pandas as pd
-data = pd.read_csv("/home/shivam_narayan/movie_project/top10K-TMDB-movies.csv")
+# Import the pandas library with alias 'pd'
+import pandas as pd  
+
+# Read the CSV file located at the specified path and store it in the 'data' DataFrame
+data = pd.read_csv("/home/shivam_narayan/movie-recommendation-project/movie-recommender-system-using-machine-learning/top10K-TMDB-movies.csv")
+
+# Print the contents of the 'data' DataFrame
 print(data)
