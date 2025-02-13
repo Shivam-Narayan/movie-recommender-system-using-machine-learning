0af414b57784612b23aa575cf7febb18c6f41ff9
Shivam Narayan <sshivam6495@gmail.com>
2025-02-13 08:20:54 +0000

    code

--- app.py
+++ app.py
@@ -3,16 +3,28 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+    try:
+        data = requests.get(url)
+        data.raise_for_status()
+        data = data.json()
+        poster_path = data.get('poster_path')
+        if poster_path:
+            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+            return full_path
+        else:
+            return "Poster not found"
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching poster: {e}")
+        return "Error fetching poster"
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+try:
+    movies = pickle.load(open("movies_list.pkl", 'rb'))
+    similarity = pickle.load(open("similarity.pkl", 'rb'))
+    movies_list = movies['title'].values
+except (FileNotFoundError, pickle.UnpicklingError) as e:
+    st.error(f"Error loading data: {e}")
+    st.stop()
 
 st.header("Movie Recommender System")
 
@@ -20,56 +32,51 @@ import streamlit.components.v1 as components
 
 imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
 
-
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-   
+try:
+    imageUrls = [
+        fetch_poster(1632),
+        fetch_poster(299536),
+        fetch_poster(17455),
+        fetch_poster(2830),
+        fetch_poster(429422),
+        fetch_poster(9722),
+        fetch_poster(13972),
+        fetch_poster(240),
+        fetch_poster(155),
+        fetch_poster(598),
+        fetch_poster(914),
+        fetch_poster(255709),
+        fetch_poster(572154)
     ]
 
+    imageCarouselComponent(imageUrls=imageUrls, height=200)
+except Exception as e:
+    st.error(f"Error creating image carousel: {e}")
 
-imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
-    for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
-        recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
-    return recommend_movie, recommend_poster
-
-
+    try:
+        index = movies[movies['title'] == movie].index[0]
+        distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+        recommend_movie = []
+        recommend_poster = []
+        for i in distance[1:6]:
+            movies_id = movies.iloc[i[0]].id
+            recommend_movie.append(movies.iloc[i[0]].title)
+            recommend_poster.append(fetch_poster(movies_id))
+        return recommend_movie, recommend_poster
+    except Exception as e:
+        st.error(f"Error recommending movies: {e}")
+        return [], []
 
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
-    with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
-    with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
-    with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
-    with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
-    with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
+    col1, col2, col3, col4, col5 = st.columns(5)
+    for idx, col in enumerate([col1, col2, col3, col4, col5]):
+        with col:
+            if idx < len(movie_name) and idx < len(movie_poster):
+                st.text(movie_name[idx])
+                st.image(movie_poster[idx])
+            else:
+                st.text("No recommendation")
